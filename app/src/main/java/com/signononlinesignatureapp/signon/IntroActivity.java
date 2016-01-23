acd77fa2a5bc0d32014c1fa9678f83c0c252d9ed
dindinpark <dindinpark@gmail.com>
2016-01-23 13:44:21 +0300

    changing sdk

--- app/src/main/java/com/signononlinesignatureapp/signon/IntroActivity.java
+++ app/src/main/java/com/signononlinesignatureapp/signon/IntroActivity.java
@@ -1,13 +1,22 @@
 package com.signononlinesignatureapp.signon;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Color;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.rgb(80,23,140));
+        }
         setContentView(R.layout.activity_intro);
     }
 }
