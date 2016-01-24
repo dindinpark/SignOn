fb685893d978beba6ee4887141ef004c476f9095
dindinpark <dindinpark@gmail.com>
2016-01-24 16:14:56 +0300

    setting

--- app/src/main/java/com/signononlinesignatureapp/signon/IntroActivity.java
+++ app/src/main/java/com/signononlinesignatureapp/signon/IntroActivity.java
@@ -1,5 +1,6 @@
 package com.signononlinesignatureapp.signon;
 
+import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -7,20 +8,29 @@ import android.os.Bundle;
 import android.graphics.Color;
 import android.view.Window;
 import android.view.WindowManager;
-
+import android.widget.Button;
+import android.view.View;
 public class IntroActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //warning removing action bar
-        //getSupportActionBar().hide();
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.rgb(80,23,140));
         }
+        Button introActivityLoginButton=(Button)findViewById(R.id.introActivityLoginButton);
+        introActivityLoginButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        startActivity(new Intent(IntroActivity.this,SettingActivity.class));
+
+                    }
+
+                }
+
+        );
         setContentView(R.layout.activity_intro);
     }
 }
