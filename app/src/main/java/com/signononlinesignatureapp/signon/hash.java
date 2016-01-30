6c801397fa3e861686d5eb71f0253f188c063059
ghadeersu <ss200200@gmail.com>
2016-01-30 13:34:56 +0300

    ECDSA and sha512  inside hash activity

--- app/src/main/java/com/signononlinesignatureapp/signon/hash.java
+++ app/src/main/java/com/signononlinesignatureapp/signon/hash.java
@@ -6,27 +6,80 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import java.math.BigInteger;
+import android.widget.Toast;
 import java.io.File;
 
 public class hash extends AppCompatActivity {
     public TextView checksum;
     public Button hashButton;
     public String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/signon";
+    Point pubkey;
+    String digest;
+    String signature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hash);
 
-        checksum= (TextView) findViewById(R.id.checksum);
-        hashButton=(Button) findViewById(R.id.hashButton);
+        checksum = (TextView) findViewById(R.id.checksum);
+        hashButton = (Button) findViewById(R.id.hashButton);
+
         File dir = new File(path);
         dir.mkdirs();
-    }
-    public void hashButtonClick(View v){
-        File file = new File (path + "/word.pdf");
-        checksum.setText(SHA512.calculateSHA512(file));
 
+
+        ///
+        try {
+            // sign ducoment
+
+             digest = SHA512.calculateSHA512(dir);
+            ECDSA app = new ECDSA();  // eliptic curve opject
+
+            BigInteger password = new BigInteger("764");
+
+            app.setdA(password);
+            signature = app.signingMessage(digest);
+
+            checksum.setText(signature);
+            pubkey = app.getQA();
+
+
+
+
+        } catch (java.lang.Exception e1) {  //
+            checksum.setText("in java.lang exciption");
+            Toast toast = Toast.makeText(getApplicationContext(), "Error please try again later", Toast.LENGTH_LONG);
+        }
+    }
+
+
+    ///
+
+    public void hashButtonClick(View v) {
+
+        /*
+        File file = new File (path + "/word.pdf");
+        checksum.setText(SHA512.calculateSHA512(file)); */
+
+        // varify document
+
+        try {
+            ECDSA app = new ECDSA();
+            app.setQA(pubkey);
+            boolean check = app.checkSignature(digest, signature);
+            if (check == true) {
+                checksum.setText(" varifcation is true");
+
+            }
+            else
+            checksum.setText("varifcation is false");
+        }
+        catch (java.lang.Exception e1){
+
+            checksum.setText("in java.lang.exception");
+
+        }
     }
 }
