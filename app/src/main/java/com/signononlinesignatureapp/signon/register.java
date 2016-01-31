b540591a2793a5dba8607b3781cf7f5a68e0440a
Kazushia <mona-cs@live.com>
2016-01-31 19:06:39 +0300

    edit on register.java

--- app/src/main/java/com/signononlinesignatureapp/signon/register.java
+++ app/src/main/java/com/signononlinesignatureapp/signon/register.java
@@ -3,7 +3,7 @@ package com.signononlinesignatureapp.signon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class register extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
