4042da7be433807c3c234895cbbd4ee0ffa830ef
Kazushia <mona-cs@live.com>
2016-01-31 16:21:00 +0300

    register activity and request signature activity

---
+++ app/src/main/java/com/signononlinesignatureapp/signon/Request_Signture.java
@@ -0,0 +1,13 @@
+package com.signononlinesignatureapp.signon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Request_Signture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request__signture);
+    }
+}
