b68775b2b0e07a8bf2eb8da4200643646b6b8da1
dindinpark <dindinpark@gmail.com>
2016-02-06 20:11:23 +0300

    clean

--- app/src/main/java/com/signononlinesignatureapp/signon/hash.java
+++ app/src/main/java/com/signononlinesignatureapp/signon/hash.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import java.math.BigInteger;
-import android.widget.Toast;
 import java.io.File;
 
 public class hash extends AppCompatActivity {
@@ -17,9 +15,7 @@ public class hash extends AppCompatActivity {
     public Button checkHashButton;
     public Button hashButton;
     public String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/signon";
-    Point pubkey;
-    String digest;
-    String signature;
+
 String hashValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,55 +31,13 @@ String hashValue;
         File file = new File (path + "/word.pdf");
         hashValue = SHA512.calculateSHA512(file);
         checksum.setText(hashValue);
-        /*
 
-
-        ///
-        try {
-            // sign ducoment
-
-             digest = SHA512.calculateSHA512(dir);
-            ECDSA app = new ECDSA();  // eliptic curve opject
-
-            BigInteger password = new BigInteger("764");
-
-            app.setdA(password);
-            signature = app.signingMessage(digest);
-
-            checksum.setText(signature);
-            pubkey = app.getQA();
-
-
-
-
-        } catch (java.lang.Exception e1) {  //
-            checksum.setText("in java.lang exciption");
-            Toast toast = Toast.makeText(getApplicationContext(), "Error please try again later", Toast.LENGTH_LONG);
-        }*/
     }
     public void hashButtonClick(View v){
         File file = new File (path + "/word.pdf");
         hashValue=SHA512.calculateSHA512(file);
         checksum.setText(hashValue);
-        /*
-        // varify document
 
-        try {
-            ECDSA app = new ECDSA();
-            app.setQA(pubkey);
-            boolean check = app.checkSignature(digest, signature);
-            if (check == true) {
-                checksum.setText(" varifcation is true");
-
-            }
-            else
-            checksum.setText("varifcation is false");
-        }
-        catch (java.lang.Exception e1){
-
-            checksum.setText("in java.lang.exception");
-
-        } */
     }
 
     public void gotoSign(View v){
