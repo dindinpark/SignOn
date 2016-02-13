eb19fd7c698cb20ee92ff610def7cd230e3776fa
Omaimah <o.alrashed93@hotmail.com>
2016-02-13 13:13:10 +0300

    Google Cloud Module ( Endpoint )  + Google App Engine

--- app/src/main/java/com/signononlinesignatureapp/signon/IntroActivity.java
+++ app/src/main/java/com/signononlinesignatureapp/signon/IntroActivity.java
@@ -1,15 +1,16 @@
 package com.signononlinesignatureapp.signon;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Pair;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.view.View;
 import android.widget.EditText;
 
 public class IntroActivity extends FragmentActivity {
@@ -23,7 +24,7 @@ public class IntroActivity extends FragmentActivity {
             window.setStatusBarColor(Color.rgb(80,23,140));
         }
 
-
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "SignOn"));
 
         setContentView(R.layout.activity_intro);
         Button introActivityLoginButton=(Button)findViewById(R.id.introActivityLoginButton);
