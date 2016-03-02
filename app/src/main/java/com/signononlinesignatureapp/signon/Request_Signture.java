1c37b244833524d085aa5c12fd33ab3ddf2b1f80
Omaimah <o.alrashed93@hotmail.com>
2016-03-02 08:16:37 +0300

    request signature (no update/delete request)

--- app/src/main/java/com/signononlinesignatureapp/signon/Request_Signture.java
+++ app/src/main/java/com/signononlinesignatureapp/signon/Request_Signture.java
@@ -4,14 +4,9 @@ import android.app.Dialog;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.ActionMode;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
@@ -20,7 +15,6 @@ import com.firebase.client.FirebaseError;
 import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,165 +22,58 @@ public class Request_Signture extends ListActivity {
 
     Intent EditRequestActivity;
     private RequestArrayAdapter mAdapter;
+    boolean flag = true;
+    static int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_request_signature);
-        EditRequestActivity= new Intent(this,main_request__edit.class);
-        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
-        getListView().setMultiChoiceModeListener(new MyMultiClickListener());
         mAdapter = new RequestArrayAdapter(this);
         setListAdapter(mAdapter);
+        counter=1;
+
     }
 
-    private class MyMultiClickListener implements AbsListView.MultiChoiceModeListener {
-
-        private ArrayList<Request> mRequestsToDelete = new ArrayList<Request>();
-
-        @Override
-        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-            mode.getMenuInflater().inflate(R.menu.context2, menu);
-            mode.setTitle("Select request to delete");
-            return true; // gives tactile feedback
-        }
-
-        @Override
-        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.context_delete:
-                    deleteSelectedItems();
-                    mode.finish();
-                    return true;
-                case R.id.context_update:
-                    updateSelectedItems();
-                    mode.finish();
-                    return true;
+    public void AddSignerButtonOnClick(View view) {
+            if (counter == 4)
+            {
+                flag = false;
+                Button btnAdd = (Button) findViewById(R.id.add_signer_button);
+                btnAdd.setEnabled(false);
+                Toast.makeText(Request_Signture.this, "you are only allowed to request 3 signers or less for each document", Toast.LENGTH_SHORT).show();
+                //start activity
             }
-            return false;
-        }
+            if (flag)
+            {
+                final Dialog dialog = new Dialog(this);
+                dialog.setTitle("Add Signer");
+                dialog.setContentView(R.layout.activity_request__signture);
+                dialog.show();
 
-        @Override
-        public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
-            Request item = (Request) getListAdapter().getItem(position);
-            if (checked) {
-                mRequestsToDelete.add(item);
-            } else {
-                mRequestsToDelete.remove(item);
-            }
-            mode.setTitle("Selected " + mRequestsToDelete.size() + " requests");
-        }
+                final EditText etEmail;
+                etEmail = (EditText) dialog.findViewById(R.id.add_dialog_signer_email);
 
-        @Override
-        public void onDestroyActionMode(ActionMode mode) {
-            // purposefully empty
-        }
+                Button btnRequest = (Button) dialog.findViewById(R.id.add_dialog_ok);
 
-        @Override
-        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-            mRequestsToDelete = new ArrayList<Request>();
-            return true;
-        }
-
-        private void deleteSelectedItems() {
-            for (Request request : mRequestsToDelete) {
-                mAdapter.removeItem(request);
-            }
-            mAdapter.notifyDataSetChanged();
-        }
-
-        private void updateSelectedItems() {
-            for (final Request request : mRequestsToDelete) {
-                EditRequestActivity.putExtra("currentRequestKey",request.getKey());
-                startActivity(EditRequestActivity);
-            }
-        }
-
-    /*@Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-
-        final Request currentRequest = (Request) getListAdapter().getItem(position);
-
-        DialogFragment df = new DialogFragment() {
-            @Override
-            public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-                View view = inflater.inflate(R.layout.activity_request__edit, container);
-                getDialog().setTitle("Update Request");
-                final Button cancelButton = (Button) view.findViewById(R.id.add_dialog_ok);
-                final EditText SignerEmailEditText = (EditText) view.findViewById(R.id.add_dialog_signer_email);
-                final EditText SignerOrderEditText = (EditText) view.findViewById(R.id.add_dialog_signer_order);
-
-                // pre-populate
-                SignerEmailEditText.setText(currentRequest.getSignerEmail());
-                SignerOrderEditText.setText(currentRequest.getOrder());
-
-                TextWatcher textWatcher = new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                    }
-
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                    }
-
-                    @Override
-                    public void afterTextChanged(Editable s) {
-                        String SignerEmail = SignerEmailEditText.getText().toString();
-                        String SignerOrder = SignerOrderEditText.getText().toString();
-                        if ( EmailExist(SignerEmail)) {
-                            mAdapter.updateItem(currentRequest, SignerEmail, SignerOrder);
-
-                        }
-                        else
-                            Toast.makeText(Request_Signture.this, "email not found", Toast.LENGTH_SHORT).show();
-                    }
-                };
-
-                SignerEmailEditText.addTextChangedListener(textWatcher);
-                SignerOrderEditText.addTextChangedListener(textWatcher);
-
-                cancelButton.setOnClickListener(new View.OnClickListener() {
+                btnRequest.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        dismiss();
+                        String email;
+                        email = etEmail.getText().toString();
+                        Request request = new Request(null, email, "", session.userkey, String.valueOf(counter), "waiting");
+                        CheckEmail(request);
+                        dialog.cancel();
                     }
                 });
-                return view;
             }
-        };
-        df.show(getFragmentManager(), "");
-
-        super.onListItemClick(l, v, position, id);*/
-    }
-
-
-        public void AddSignerButtonOnClick(View view) {
-            final Dialog dialog = new Dialog(this);
-            dialog.setTitle("Add Signer");
-            dialog.setContentView(R.layout.activity_request__signture);
-            dialog.show();
-
-            final EditText etEmail, etOrder;
-            etEmail = (EditText) dialog.findViewById(R.id.add_dialog_signer_email);
-            etOrder = (EditText) dialog.findViewById(R.id.add_dialog_signer_order);
-
-            Button btnRequest = (Button) dialog.findViewById(R.id.add_dialog_ok);
-
-            btnRequest.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String email, order;
-                    email = etEmail.getText().toString();
-                    order = etOrder.getText().toString();
-                    Request request = new Request(null, email, "", session.userkey, order, "waiting");
-                    CheckEmail(request);
-                    dialog.cancel();
-                }
-            });
 
         }
+
+    public void DoneButtonOnClick (View view)
+    {
+        //start activity
+    }
 
 
         private void CheckEmail(final Request request) {
@@ -228,6 +115,7 @@ public class Request_Signture extends ListActivity {
             newRequest.put("status", "waiting");
             reqRef.push().setValue(newRequest);
             Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_SHORT).show();
+            counter++;
 
 
         }
